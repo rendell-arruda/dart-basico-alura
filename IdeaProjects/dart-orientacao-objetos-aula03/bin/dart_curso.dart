489aafbfe08ca9b89ee7e62d2dc0a4fedd0ceb0a
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-11 15:12:02 -0300

    aula herança

--- IdeaProjects/dart-orientacao-objetos-aula03/bin/dart_curso.dart
+++ IdeaProjects/dart-orientacao-objetos-aula03/bin/dart_curso.dart
@@ -1,8 +1,8 @@
 void main() {
-  String nome = "Laranja";
-  double peso = 100.2;
-  String cor = "Verde e Amarela";
-  String sabor = "Doce e cítrica";
+  String nome = "Uva";
+  double peso = 40.2;
+  String cor = "Verde";
+  String sabor = "Doce";
   int diasDesdeColheita = 40;
   bool? isMadura;
 
@@ -37,16 +37,15 @@ int funcQuantosDiasMadura(int dias) {
   return quantosDiasFaltam;
 }
 
-class Fruta {
-  String nome;
-  double peso;
-  String cor;
+class Fruta extends Alimento {
   String sabor;
   int diasDesdeColheita;
   bool? isMadura;
 
-  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita,
-      {this.isMadura});
+  Fruta(
+      String nome, double peso, String cor, this.sabor, this.diasDesdeColheita,
+      {this.isMadura})
+      : super(nome, peso, cor);
 
   void estaMadura(int diasParaMadura) {
     isMadura = diasDesdeColheita >= diasParaMadura;
@@ -54,4 +53,59 @@ class Fruta {
         ' $diasParaMadura dias para poder comer!'
         'Ela está Madura? $isMadura');
   }
+
+  void fazerSuco() {
+    print('Você fez um otimo suco de $nome');
+  }
+}
+
+class Alimento {
+  String nome;
+  double peso;
+  String cor;
+
+  Alimento(this.nome, this.peso, this.cor);
+
+  void printAlimento() {
+    print('Este(a) alimento $nome pesa $peso gramas e é $cor .');
+  }
+}
+
+class Legumes extends Alimento {
+  bool isPrecisaCozinhar;
+
+  Legumes(String nome, double peso, String cor, this.isPrecisaCozinhar)
+      : super(nome, peso, cor);
+
+  void cozinhar() {
+    if (isPrecisaCozinhar) {
+      print('Pronto, o $nome está cozinhando');
+    } else {
+      print('Nem precisa cozinhar');
+    }
+  }
+}
+
+class Citricas extends Fruta {
+  double nivelAzedo;
+
+  Citricas(String nome, double peso, String cor,String sabor, int diasDesdeColheita,
+      this.nivelAzedo)
+      : super(nome, peso, cor, sabor, diasDesdeColheita);
+
+  void existeRefri(bool existe){
+    if(existe){
+      print('Existe refrigerante de $nome.');
+    }else{
+      print('Não existe refri de $nome.');
+    }
+  }
+}
+
+class Nozes extends Fruta {
+
+  double porcentagemOleoNatural;
+
+  Nozes(nome, peso, cor, this.diasDesdeColheita, this.isMadura,
+      this.porcentagemOleoNatural);
 }
