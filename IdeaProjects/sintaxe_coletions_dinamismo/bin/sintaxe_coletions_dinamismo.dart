08f2687113a7706a5ac4a03bf3aca8a3a034a5b5
Rendell Arruda <rendellarruda3@gmail.com>
2023-01-12 19:18:35 -0300

    äula Sets

--- IdeaProjects/sintaxe_coletions_dinamismo/bin/sintaxe_coletions_dinamismo.dart
+++ IdeaProjects/sintaxe_coletions_dinamismo/bin/sintaxe_coletions_dinamismo.dart
@@ -1,24 +1,32 @@
 void main(List<String> arguments) {
-  escolherMeioTransporteEnum(Transporte.carro);
+  escolherMeioTransporte(Transporte.andando);
+  Set<String> registrosVisitados = <String>{};
+  registrosVisitados = registrarDestinos('São Paulo', registrosVisitados);
+  registrosVisitados = registrarDestinos('Belém', registrosVisitados);
+  registrosVisitados = registrarDestinos('Rio de janeiro', registrosVisitados);
+  registrosVisitados = registrarDestinos('São Paulo', registrosVisitados);
+  registrosVisitados = registrarDestinos('Recife', registrosVisitados);
+  print(registrosVisitados);
 }
 
-void escolherMeioTransporte(int locomocao) {
-  if (locomocao == 0) {
-    print('Vou de Carro para a aventura');
-  } else if (locomocao == 1) {
-    print('Vou de Bike para a aventura');
-  } else {
-    print('Vou para a aventura');
-  }
+Set<String> registrarDestinos(String destino, Set<String> banco) {
+  banco.add(destino);
+  return banco;
 }
 
-void escolherMeioTransporteEnum(Transporte locomocao) {
-  if (locomocao == Transporte.carro) {
-    print('Vou de Carro para a aventura');
-  } else if (locomocao == Transporte.bike) {
-    print('Vou de Bike para a aventura');
-  } else {
-    print('Vou para a aventura');
+void escolherMeioTransporte(Transporte locomocao) {
+  switch (locomocao) {
+    case Transporte.carro:
+      print('Vou de Carro para a aventura');
+      break;
+    case Transporte.bike:
+      print('Vou de Bike para a aventura');
+      break;
+    case Transporte.skate:
+      print('Vou de Skate para a aventura');
+      break;
+    default:
+      print('Estou indo para a aventura, isso é o que importa!');
   }
 }
 
